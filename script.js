61cea18bf66a1a40693f70c7941cc5168ce71745
vishnuKr006 <162488939+vishnuKr006@users.noreply.github.com>
2024-04-10 14:38:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Graphic Designer', 'Software Developer', 'Programmer', 'Ethical Hacker', 'Full Stack Developer', '&amp; UI-UX Designer'],
+    typeSpeed: 90,
+    BackSpeed: 100,
+    loop: true
+});
